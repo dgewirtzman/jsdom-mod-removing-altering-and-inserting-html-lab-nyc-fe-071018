1e539a5c24af3bd11ed736029c8c7e7a272b07bd
David Gewirtzman <dgewirtzman@playbill.com>
2018-08-08 15:37:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 /* Enter the code to remove the main node element under this comment */
 main.remove()
+<<<<<<< HEAD
 
 /* Create your new element here and assign it to newHeader */
 const newHeader = document.createElement('h1');
 newHeader.id = "victory";
 newHeader.innerText = "Woot is the champion";
+=======
+/* Create your new element here and assign it to newHeader */
+const newHeader = null;
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+body.appendChild(newHeader);
+>>>>>>> c5177fcda3d13e556a823c6bf569a6ab0ed266b4
